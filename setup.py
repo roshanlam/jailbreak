c40eaede9d9f7fe1483d372e45f80b0d8f16ab12
Roshan Lamichhane <lamichhaner40@gmail.com>
2021-03-25 12:50:58 -0400

    Setting Things Up

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os, time
+
+os.system("cls" if os.name == "nt" else "clear")
+print("""
+
+    \tWelcome To Roshan's Jailbreak.\n\tLet's start breaking stuff :)
+    \tChoose one of the following options:\n\t[1]: Check if the dependencies are installed.\n\t[2]: Install the dependencies\n\t[3]: Start the jailbreak process.
+""")
+
+userInput = input("\t")
